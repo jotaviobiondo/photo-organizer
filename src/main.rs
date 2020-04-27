f03e16d38cb0d3dd85a39858b1f004bba055a3e5
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-27 17:05:03 -0300

    Refactoring and formatting code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use chrono::{DateTime, TimeZone, Utc};
 use exif::{Field, Tag, Value};
 use structopt::StructOpt;
 
+const IMAGE_EXTENSIONS: [&'static str; 3] = ["jpg", "jpeg", "png"];
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "photo-organizer", about = "Organize photos of a folder by year/month")]
 struct Cli {
@@ -18,28 +20,31 @@ struct Cli {
 }
 
 
-fn has_extensions(path: &PathBuf, extensions: &Vec<&str>) -> bool {
-    path.extension()
-        .map(|x| x.to_str())
-        .flatten()
-        .map(|x| x.to_lowercase())
-        .filter(|x| extensions.contains(&x.as_str()))
-        .is_some()
-}
-
 fn find_image_files_from_current_dir(dir: &PathBuf) -> Result<Vec<PathBuf>, String> {
-    let extensions = vec!["jpg", "jpeg", "png"];
-
     dir.read_dir()
         .map(|files| {
             files
                 .flatten()
-                .map(|x: DirEntry| x.path())
-                .filter(|x| x.is_file())
-                .filter(|x| has_extensions(x, &extensions))
+                .map(|entry: DirEntry| entry.path())
+                .filter(|path| path.is_file())
+                .filter(is_image)
                 .collect()
         })
         .map_err(|err| err.to_string())
+}
+
+fn is_image(path: &PathBuf) -> bool {
+    path.extension()
+        .map(|extension| extension.to_str())
+        .flatten()
+        .map(|extension| extension.to_lowercase())
+        .filter(|extension| IMAGE_EXTENSIONS.contains(&extension.as_str()))
+        .is_some()
+}
+
+fn get_photo_date_time(path: &PathBuf) -> Option<DateTime<Utc>> {
+    get_exif_date_time_original(path)
+        .or(get_file_last_modified_datetime(path))
 }
 
 fn get_exif_date_time_original(path: &PathBuf) -> Option<DateTime<Utc>> {
@@ -54,10 +59,6 @@ fn get_exif_date_time_original(path: &PathBuf) -> Option<DateTime<Utc>> {
         .flatten()
 }
 
-fn exif_to_date_time(exif_date_time: exif::DateTime) -> DateTime<Utc> {
-    Utc.datetime_from_str(exif_date_time.to_string().as_str(), "%Y-%m-%d %H:%M:%S").unwrap()
-}
-
 fn ascii_to_date_time(field: &Field) -> Option<DateTime<Utc>> {
     match field.value {
         Value::Ascii(ref vec) => {
@@ -69,26 +70,22 @@ fn ascii_to_date_time(field: &Field) -> Option<DateTime<Utc>> {
     }
 }
 
+fn exif_to_date_time(exif_date_time: exif::DateTime) -> DateTime<Utc> {
+    Utc.datetime_from_str(exif_date_time.to_string().as_str(), "%Y-%m-%d %H:%M:%S").unwrap()
+}
+
 fn get_file_last_modified_datetime(path: &PathBuf) -> Option<DateTime<Utc>> {
-    path.metadata().ok()
+    path.metadata()
+        .ok()
         .map(|metadata| metadata.modified().ok())
         .flatten()
         .map(|time_modified| DateTime::from(time_modified))
 }
 
-fn get_photo_date_time(path: &PathBuf) -> Option<DateTime<Utc>> {
-    get_exif_date_time_original(path)
-        .or(get_file_last_modified_datetime(path))
-}
-
 fn main() {
     let args: Cli = Cli::from_args();
 
-    println!("{:?}", args);
-
-    let Cli { dir } = args;
-
-    let files = find_image_files_from_current_dir(&dir).unwrap();
+    let files = find_image_files_from_current_dir(&args.dir).unwrap();
 
     println!("Files:");
     for entry in &files {
