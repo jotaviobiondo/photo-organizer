699603c11fe9ee47ed5f9b50c28025793c282f51
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-27 17:08:02 -0300

    Validating input directory

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,31 @@ use structopt::StructOpt;
 const IMAGE_EXTENSIONS: [&'static str; 3] = ["jpg", "jpeg", "png"];
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "photo-organizer", about = "Organize photos of a folder by year/month")]
+#[structopt(
+    name = "photo-organizer",
+    about = "Organize photos of a folder by year/month"
+)]
 struct Cli {
     #[structopt(
-    help = "The directory containing the photos to organize. Default is the current directory.",
-    default_value = ".",
-    parse(from_os_str)
+        help = "The directory containing the photos to organize. Default is the current directory.",
+        default_value = ".",
+        parse(from_os_str),
+        validator(dir_validator)
     )]
-    dir: PathBuf
+    dir: PathBuf,
 }
 
+fn dir_validator(dir_str: String) -> Result<(), String> {
+    let dir = Path::new(dir_str.as_str());
+
+    if !dir.exists() {
+        Err(format!("The directory '{}' does not exist.", dir.display()))
+    } else if !dir.is_dir() {
+        Err(format!("'{}' is not a directory.", dir.display()))
+    } else {
+        Ok(())
+    }
+}
 
 fn find_image_files_from_current_dir(dir: &PathBuf) -> Result<Vec<PathBuf>, String> {
     dir.read_dir()
@@ -43,8 +58,7 @@ fn is_image(path: &PathBuf) -> bool {
 }
 
 fn get_photo_date_time(path: &PathBuf) -> Option<DateTime<Utc>> {
-    get_exif_date_time_original(path)
-        .or(get_file_last_modified_datetime(path))
+    get_exif_date_time_original(path).or(get_file_last_modified_datetime(path))
 }
 
 fn get_exif_date_time_original(path: &PathBuf) -> Option<DateTime<Utc>> {
@@ -66,12 +80,13 @@ fn ascii_to_date_time(field: &Field) -> Option<DateTime<Utc>> {
 
             Some(exif_to_date_time(exif_date_time))
         }
-        _ => None
+        _ => None,
     }
 }
 
 fn exif_to_date_time(exif_date_time: exif::DateTime) -> DateTime<Utc> {
-    Utc.datetime_from_str(exif_date_time.to_string().as_str(), "%Y-%m-%d %H:%M:%S").unwrap()
+    Utc.datetime_from_str(exif_date_time.to_string().as_str(), "%Y-%m-%d %H:%M:%S")
+        .unwrap()
 }
 
 fn get_file_last_modified_datetime(path: &PathBuf) -> Option<DateTime<Utc>> {
